65b7efe1508441b8d7541374c40d27f7d26bbfd5
HarlowKnott18 <hknott@ucsc.edu>
2024-04-22 15:43:36 -0700

    updated screenshots

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Rock-paper-scissors.
+ *
+ * An implementation of the game rock-paper-scissors.
+ *
+ * @author Harlow Knott and Maya Pahre
+ * @since  April 2024
+ */
 
-// Constants
+/** jshint {inline configuration here} */
+//Set rules of the game:
+    //rock beats scissors, paper beats rock, and scissors beat paper
+//Get player input by prompting player to pick rock, paper, or scissors
+//Read player's input of rock, paper, or scissors
+//Generate a random number to represent the computer’s choice 
+    //(1=rock, 2=paper, 3=scissors)
+    //Convert random number into corresponding choice
+//Compare player’s choice with computer’s choice to determine winner
+//Implement game logic to determine outcome:
+    //rock beats scissors, paper beats rock, and scissors beat paper, if the same they tie
+//Display result of the game 
+    //(you win, you lose, you tie)
+//Ask if they want to play again and repeat step 2-8 if they say yes
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
---
+++ lab4/js/lab4.js
@@ -0,0 +1,19 @@
+/**
+ * How to clean a litter box
+ *
+ * instructions on how to clean a litter box
+ *
+ * @author Harlow Knott and Maya Pahre
+ * @since  April 2024
+ */
+
+/** jshint {inline configuration here} */
+//Remove the feline from the premises 
+    //Grab litter scooper (aka pooper scooper)
+    //Grab an empty grocery bag 
+//Use litter scooper to remove and sift through any solid waste
+    //Put the waste into the grocery bag
+        //Take grocery bag of litter out to the garbage can
+//Grab new litter to replace old litter that was scooped out
+    //Place new litter into the litter box
+//Bring the cat back to a fresh litter box
