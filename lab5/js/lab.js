09e597d17f442cce04f642892d7ba3ae7d567c52
HarlowKnott18 <hknott@ucsc.edu>
2024-04-25 14:25:44 -0700

    lab 5 data type and variable update

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// *
+//**
+//* 
+ //*Toyota Yaris Car Code
+ //* coding datatype and variables for Toyota Yaris
+ //*
+ //* @author Harlow Knott and Maya Pahre
+//* @since  April 2024
+ //* liscense: public domain
+ 
+//Define Variables;
+make = "Toyota";
+model = "Yaris";
+color = "Red";
+year = 2018;
 
-// Constants
+//calculate 
+age = 2024 - year;
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
