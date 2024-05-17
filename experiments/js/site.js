f9bc12e1f444077d8de9b0a5ee1d98fb659773e3
HarlowKnott18 <hknott@ucsc.edu>
2024-05-16 21:28:12 -0700

    lab 10

---
+++ experiments/js/site.js
@@ -0,0 +1,18 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function getNameAndDisplay(){
+    let name = prompt("NAME NOW!!")
+    $('#title').html('Hello,' + name);
+  }
+  
+  $("#my-button").click(gerNameAndDisplay);
+    console.log("click")
+    
+  
